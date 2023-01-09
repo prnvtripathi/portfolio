9dd72f2ba1f0434dbd40b66e69830e4de5059c39
Pranav Tripathi <tripathipranav14@gmail.com>
2023-01-09 13:28:31 +0530

    React Router started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './pages/Layout/Layout';
+import Home from './pages/Home/Home';
+import Blogs from './pages/Blog/Blog';
+import NoPage from './pages/NoPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='*' element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,6 @@
+const Blog = () => {
+    return <h1>Blogs</h1>;
+  };
+  
+  export default Blog;
+  
