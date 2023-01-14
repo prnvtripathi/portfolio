2fbeed8f0e2bebb0dfa2f1b81fc9ffd45974fe8d
Pranav Tripathi <tripathipranav14@gmail.com>
2023-01-14 23:21:26 +0530

    responsiveness done

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './Layout.css'
 import { Outlet, NavLink } from "react-router-dom";
 
@@ -5,13 +6,24 @@ const Layout = () => {
 
     const resumeLink = 'https://drive.google.com/file/d/1al5HUHBLi2Z5T_87vVc4V95CTQG6JPO3/view?usp=sharing'
 
+    const [expanded, setExpanded] = useState(false)
+
     return (
         <>
             <nav>
+                <input id="toggle" type="checkbox" onClick={() => {
+                    setExpanded(!expanded)
+                }} />
+
+                <label htmlFor="toggle" className="hamburger">
+                    <div className="top-bun"></div>
+                    <div className="meat"></div>
+                    <div className="bottom-bun"></div>
+                </label>
                 <div className='brand'>
                     <a href='/'>prnvtripathi</a>
                 </div>
-                <ul>
+                <ul className={expanded ? "expanded" : ""}>
                     <li>
                         <NavLink to="/" className='nav-links' activeClassName='active'>Home</NavLink>
                     </li>
